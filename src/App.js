8f4e573de6cf031e1591a9eb3e6913c79928fc91
Carlo Cadiz <carlocadiz71@gmail.com>
2019-08-16 23:32:42 -0400

    changed app component to use hooks

--- src/App.js
+++ src/App.js
@@ -1,55 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import youtube from './api/youtube';
 import { SearchBar, VideoList, VideoDetail } from './components';
 
 import { Grid } from '@material-ui/core';
 
-class App extends React.Component {
+const App= () => {
 
-    state = {
-        videos: [],
-        selectedVideo: null,
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+ const handleSubmit = async (searchTerm) => {
+    const response = await youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: 'AIzaSyCeUyBTSJsMQpqr_fgmipvQePk7Vx9iIc8',
+        q: searchTerm,
       }
-      
-    handleSubmit = async (searchTerm) => {
-        const response = await youtube.get('search', {
-          params: {
-            part: 'snippet',
-            maxResults: 5,
-            key: 'AIzaSyCeUyBTSJsMQpqr_fgmipvQePk7Vx9iIc8',
-            q: searchTerm,
-          }
-        });
-    
-        this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
-      }
+    });
 
-    onVideoSelect = (video) => {
-        this.setState({ selectedVideo: video });
-      }
+     setVideos(response.data.items);
+     setSelectedVideo(response.data.items[0]);
+  }
 
-	render() {
-
-      const { selectedVideo, videos } = this.state;
-
-      return (
-        <Grid style={{ justifyContent: 'center' }} container spacing={10}>
+  return (
+    <Grid style={{ justifyContent: 'center' }} container spacing={10}>
           <Grid item xs={11}>
             <Grid container spacing={10}>
               <Grid item xs={12}>
-              	<SearchBar onFormSubmit={this.handleSubmit}/>
+              	<SearchBar onFormSubmit={handleSubmit}/>
               </Grid>
               <Grid item xs={8}>
               	<VideoDetail video={selectedVideo}/>
               </Grid>
               <Grid item xs={4}>
-              	<VideoList videos={videos} onVideoSelect={this.onVideoSelect}/>
+              	<VideoList videos={videos} onVideoSelect={setSelectedVideo}/>
               </Grid>
             </Grid>
           </Grid>
         </Grid>
-      );
-    }
+  )
 }
+
+
+// class App extends React.Component {
+
+//     state = {
+//         videos: [],
+//         selectedVideo: null,
+//       }
+      
+//     handleSubmit = async (searchTerm) => {
+//         const response = await youtube.get('search', {
+//           params: {
+//             part: 'snippet',
+//             maxResults: 5,
+//             key: 'AIzaSyCeUyBTSJsMQpqr_fgmipvQePk7Vx9iIc8',
+//             q: searchTerm,
+//           }
+//         });
+    
+//         this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
+//       }
+
+//     onVideoSelect = (video) => {
+//         this.setState({ selectedVideo: video });
+//       }
+
+// 	render() {
+
+//       const { selectedVideo, videos } = this.state;
+
+//       return (
+//         <Grid style={{ justifyContent: 'center' }} container spacing={10}>
+//           <Grid item xs={11}>
+//             <Grid container spacing={10}>
+//               <Grid item xs={12}>
+//               	<SearchBar onFormSubmit={this.handleSubmit}/>
+//               </Grid>
+//               <Grid item xs={8}>
+//               	<VideoDetail video={selectedVideo}/>
+//               </Grid>
+//               <Grid item xs={4}>
+//               	<VideoList videos={videos} onVideoSelect={this.onVideoSelect}/>
+//               </Grid>
+//             </Grid>
+//           </Grid>
+//         </Grid>
+//       );
+//     }
+// }
 
 export default App;
